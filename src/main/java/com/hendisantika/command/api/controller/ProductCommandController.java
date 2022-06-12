e7e9f8bf4856768433f9598352a3508b99b7af7b
Hendi Santika <hendisantika@gmail.com>
2022-06-12 19:42:00 +0700

    #19 - Add ProductCommandController

---
+++ src/main/java/com/hendisantika/command/api/controller/ProductCommandController.java
@@ -0,0 +1,25 @@
+package com.hendisantika.command.api.controller;
+
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample8
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/06/22
+ * Time: 19.41
+ */
+@RestController
+@RequestMapping("/products")
+public class ProductCommandController {
+
+    private final CommandGateway commandGateway;
+
+    public ProductCommandController(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+}

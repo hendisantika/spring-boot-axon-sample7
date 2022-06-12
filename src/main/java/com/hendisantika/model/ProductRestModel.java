cffb5baa74c0ec249bd4bbfb35423cf6e61a9590
Hendi Santika <hendisantika@gmail.com>
2022-06-12 19:33:49 +0700

    #7 - Add ProductRestModel

---
+++ src/main/java/com/hendisantika/model/ProductRestModel.java
@@ -0,0 +1,23 @@
+package com.hendisantika.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample8
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/06/22
+ * Time: 19.33
+ */
+@Data
+@Builder
+public class ProductRestModel {
+    private String name;
+    private BigDecimal price;
+    private Integer quantity;
+}

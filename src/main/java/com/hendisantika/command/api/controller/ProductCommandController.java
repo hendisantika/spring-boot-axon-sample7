0355b620d7e6694610d3c9382956620c814a5495
Hendi Santika <hendisantika@gmail.com>
2022-06-12 19:42:21 +0700

    #20 - Add addProduct in ProductCommandController

--- src/main/java/com/hendisantika/command/api/controller/ProductCommandController.java
+++ src/main/java/com/hendisantika/command/api/controller/ProductCommandController.java
@@ -1,8 +1,14 @@
 package com.hendisantika.command.api.controller;
 
+import com.hendisantika.command.api.command.CreateProductCommand;
+import com.hendisantika.command.api.model.ProductRestModel;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +27,18 @@ public class ProductCommandController {
 
     public ProductCommandController(CommandGateway commandGateway) {
         this.commandGateway = commandGateway;
+    }
+
+    @PostMapping
+    public String addProduct(@RequestBody ProductRestModel productRestModel) {
+        CreateProductCommand createProductCommand =
+                CreateProductCommand.builder()
+                        .productId(UUID.randomUUID().toString())
+                        .name(productRestModel.getName())
+                        .price(productRestModel.getPrice())
+                        .quantity(productRestModel.getQuantity())
+                        .build();
+        String result = commandGateway.sendAndWait(createProductCommand);
+        return result;
     }
 }

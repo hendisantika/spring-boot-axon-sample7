d490c73b54018df56247a3d8133c3f474ab2bb2a
Hendi Santika <hendisantika@gmail.com>
2022-06-12 19:34:58 +0700

    #9 - Add ProductCreatedEvent

---
+++ src/main/java/com/hendisantika/event/ProductCreatedEvent.java
@@ -0,0 +1,29 @@
+package com.hendisantika.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample8
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/06/22
+ * Time: 19.34
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductCreatedEvent {
+
+    private String productId;
+    private String name;
+    private BigDecimal price;
+    private Integer quantity;
+}

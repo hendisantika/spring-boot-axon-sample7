e2ea506ad3eda01b3676ac63874592cf9cf9ae31
Hendi Santika <hendisantika@gmail.com>
2022-06-12 19:34:28 +0700

    #8 - Add ProductServiceEventsErrorHandler

---
+++ src/main/java/com/hendisantika/exception/ProductServiceEventsErrorHandler.java
@@ -0,0 +1,21 @@
+package com.hendisantika.exception;
+
+import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.eventhandling.EventMessageHandler;
+import org.axonframework.eventhandling.ListenerInvocationErrorHandler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample8
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/06/22
+ * Time: 19.34
+ */
+public class ProductServiceEventsErrorHandler implements ListenerInvocationErrorHandler {
+    @Override
+    public void onError(Exception exception, EventMessage<?> event, EventMessageHandler eventHandler) throws Exception {
+        throw exception;
+    }
+}
